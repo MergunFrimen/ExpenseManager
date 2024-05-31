6c2362782b685a7b5bb7b77cfd91ce8b7fbe501e
Dominik Tichy <tichydominik451@gmail.com>
2024-05-31 22:01:01 +0200

    added datetime provider

--- src/ExpenseManager.Infrastructure/DependencyInjection.cs
+++ src/ExpenseManager.Infrastructure/DependencyInjection.cs
@@ -1,16 +1,23 @@
 using ExpenseManager.Application.Common.Interfaces.Authentication;
+using ExpenseManager.Application.Common.Interfaces.Services;
 using ExpenseManager.Application.Services.Authentication;
 using ExpenseManager.Infrastructure.Authentication;
+using ExpenseManager.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ExpenseManager.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+        ConfigurationManager configurationManager)
     {
+        services.Configure<JwtSettings>(configurationManager.GetSection(JwtSettings.SectionName));
+
         services.AddScoped<IAuthenticationService, AuthenticationService>();
         services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
 
         return services;
     }
