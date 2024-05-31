955fdf16319bdd9f07dec712ee93fac123a78f2e
Dominik Tichy <tichydominik451@gmail.com>
2024-05-31 22:02:20 +0200

    added mock token generation to auth service

--- src/ExpenseManager.Application/Services/Authentication/AuthenticationService.cs
+++ src/ExpenseManager.Application/Services/Authentication/AuthenticationService.cs
@@ -1,10 +1,20 @@
+using ExpenseManager.Application.Common.Interfaces.Authentication;
+
 namespace ExpenseManager.Application.Services.Authentication;
 
-public class AuthenticationService: IAuthenticationService
+public class AuthenticationService(IJwtTokenGenerator tokenGenerator): IAuthenticationService
 {
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
-        return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, "token");
+        // Check if user already exists with arguments
+        
+        // Generate user (gets the user's ID)
+        var userId = Guid.NewGuid();
+        
+        // Create JWT token
+        var token = tokenGenerator.GenerateToken(userId, firstName, lastName);
+        
+        return new AuthenticationResult(userId, firstName, lastName, email, token);
     }
 
     public AuthenticationResult Login(string email, string password)
