c1966f52723c9d1f01f5f08d5fd2b2333584efb6
Dominik Tichy <tichydominik451@gmail.com>
2024-05-31 18:27:49 +0200

    added auth service

--- src/ExpenseManager.Api/Program.cs
+++ src/ExpenseManager.Api/Program.cs
@@ -1,17 +1,23 @@
+using ExpenseManager.Application.Services.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 {
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+    // builder.Services.AddEndpointsApiExplorer();
+    // builder.Services.AddSwaggerGen();
+    builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+    builder.Services.AddControllers();
 }
 
 var app = builder.Build();
 {
     if (app.Environment.IsDevelopment())
     {
-        app.UseSwagger();
-        app.UseSwaggerUI();
+        // app.UseSwagger();
+        // app.UseSwaggerUI();
     }
-
+    
     app.UseHttpsRedirection();
+    app.MapControllers();
+    
     app.Run();
 }
