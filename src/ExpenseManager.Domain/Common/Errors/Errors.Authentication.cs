2736c54fef4ee0e214fd8ad04b9e62b418ae144a
Dominik Tichy <tichydominik451@gmail.com>
2024-06-02 17:56:58 +0200

    custom errors

---
+++ src/ExpenseManager.Domain/Common/Errors/Errors.Authentication.cs
@@ -0,0 +1,13 @@
+using ErrorOr;
+
+namespace ExpenseManager.Domain.Common.Errors;
+
+public static partial class Errors
+{
+    public static class Authentication
+    {
+        public static Error InvalidCredentials => Error.Validation(
+            "User.InvalidCredentials",
+            "Invalid credentials.");
+    }
+}
