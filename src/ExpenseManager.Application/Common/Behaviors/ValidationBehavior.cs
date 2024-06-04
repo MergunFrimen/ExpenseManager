53d4d0e079bfa1b5fa51a48e4961efbd931420c1
Dominik Tichy <tichydominik451@gmail.com>
2024-06-05 00:50:05 +0200

    removed command/query validatoin

--- src/ExpenseManager.Application/Common/Behaviors/ValidationBehavior.cs
+++ src/ExpenseManager.Application/Common/Behaviors/ValidationBehavior.cs
@@ -16,69 +16,17 @@ public class ValidationBehavior<TRequest, TResponse> :
         _validator = validator;
     }
 
-    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
     {
-        if (_validator is null)
-        {
-            return await next();
-        }
+        if (_validator is null) return await next();
 
         var validationResult = await _validator.ValidateAsync(request, cancellationToken);
 
-        if (validationResult.IsValid)
-        {
-            return await next();
-        }
+        if (validationResult.IsValid) return await next();
 
-        var errors = validationResult.Errors.ConvertAll(validationFailure => Error.Validation(validationFailure.PropertyName, validationFailure.ErrorMessage));
+        var errors = validationResult.Errors.ConvertAll(validationFailure =>
+            Error.Validation(validationFailure.PropertyName, validationFailure.ErrorMessage));
         return (dynamic)errors;
     }
 }
-
-// public class CommandValidationBehavior<TRequest, TResponse>(IValidator<TRequest>? validator = null) :
-//     IPipelineBehavior<TRequest, ErrorOr<TResponse>>
-//     where TRequest : ICommand<TResponse>
-// {
-//     public async Task<ErrorOr<TResponse>> Handle(TRequest request, RequestHandlerDelegate<ErrorOr<TResponse>> next, CancellationToken cancellationToken)
-//     {
-//         if (validator is null)
-//         {
-//             return await next();
-//         }
-//
-//         var validationResult = await validator.ValidateAsync(request, cancellationToken);
-//
-//         if (!validationResult.IsValid)
-//             return await next();
-//
-//         var errors = validationResult.Errors.ConvertAll(validationFailure => Error.Validation(
-//             validationFailure.PropertyName,
-//             validationFailure.ErrorMessage));
-//
-//         return errors;
-//     }
-// }
-//
-// public class QueryValidationBehavior<TRequest, TResponse>(IValidator<TRequest>? validator = null) :
-//     IPipelineBehavior<TRequest, ErrorOr<TResponse>>
-//     where TRequest : IQuery<TResponse>
-// {
-//     public async Task<ErrorOr<TResponse>> Handle(TRequest request, RequestHandlerDelegate<ErrorOr<TResponse>> next, CancellationToken cancellationToken)
-//     {
-//         if (validator is null)
-//         {
-//             return await next();
-//         }
-//
-//         var validationResult = await validator.ValidateAsync(request, cancellationToken);
-//
-//         if (!validationResult.IsValid)
-//             return await next();
-//
-//         var errors = validationResult.Errors.ConvertAll(validationFailure => Error.Validation(
-//             validationFailure.PropertyName,
-//             validationFailure.ErrorMessage));
-//
-//         return errors;
-//     }
-// }
