43b06b03330b80de855164a613f98a2f60c5e0d0
Dominik Tichy <tichydominik451@gmail.com>
2024-06-06 02:24:09 +0200

    migration

---
+++ src/ExpenseManager.Infrastructure/Migrations/20240606002332_InitialCreate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Category = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => new { x.Id, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_Transactions_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_UserId",
+                table: "Transactions",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- src/ExpenseManager.Infrastructure/Persistence/Configurations/UserConfigurations.cs
+++ src/ExpenseManager.Infrastructure/Persistence/Configurations/UserConfigurations.cs
@@ -9,34 +9,68 @@ public sealed class UserConfigurations : IEntityTypeConfiguration<User>
     public void Configure(EntityTypeBuilder<User> builder)
     {
         ConfigureUsersTable(builder);
+        ConfigureTransactionTable(builder);
     }
 
-    private void ConfigureUsersTable(EntityTypeBuilder<User> builder)
+    private void ConfigureUsersTable(EntityTypeBuilder<User> usersBuilder)
     {
-        builder
+        usersBuilder
             .ToTable("Users");
 
-        builder
+        usersBuilder
             .HasKey(u => u.Id);
 
-        builder
+        usersBuilder
             .Property(u => u.Id)
             .ValueGeneratedNever()
             .HasConversion<Guid>();
 
-        builder
+        usersBuilder
             .Property(u => u.FirstName)
             .HasMaxLength(50);
 
-        builder
+        usersBuilder
             .Property(u => u.LastName)
             .HasMaxLength(50);
 
-        builder
+        usersBuilder
             .Property(u => u.Email)
             .HasMaxLength(150);
 
-        builder
+        usersBuilder
             .Property(u => u.Password);
+    }
+
+    private void ConfigureTransactionTable(EntityTypeBuilder<User> usersBuilder)
+    {
+        usersBuilder.OwnsMany(user => user.Transactions, transactionsBuilder =>
+        {
+            transactionsBuilder
+                .ToTable("Transactions");
+            transactionsBuilder
+                .HasKey("Id", "UserId");
+            transactionsBuilder
+                .WithOwner()
+                .HasForeignKey("UserId");
+           
+            transactionsBuilder
+                .Property(s => s.Id)
+                .ValueGeneratedNever()
+                .HasConversion<Guid>();
+            transactionsBuilder
+                .Property(s => s.UserId)
+                .ValueGeneratedNever()
+                .HasConversion<Guid>();
+            transactionsBuilder
+                .Property(s => s.Type);
+            transactionsBuilder
+                .Property(s => s.Category);
+            transactionsBuilder
+                .Property(s => s.Description);
+            transactionsBuilder
+                .Property(s => s.Price);
+            transactionsBuilder
+                .Property(s => s.Date);
+        });
     }
 }
