3625203ca3c802d9cb9fa2ce459da64154e949ea
Dominik Tichy <tichydominik451@gmail.com>
2024-06-06 20:35:28 +0200

    added style

--- src/ExpenseManager.Presentation/Client/src/routes/AuthRootPage.tsx
+++ src/ExpenseManager.Presentation/Client/src/routes/AuthRootPage.tsx
@@ -2,12 +2,23 @@ import {Outlet} from "react-router-dom";
 import BaseLayout from "@/layouts/BaseLayout.tsx";
 
 export default function AuthRootPage() {
-    // TODO: redirect to login page if not authenticated
-    
     return (
         <BaseLayout>
             <h1>AuthRootPage</h1>
-            <Outlet/>
+            {/*<div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">*/}
+            {/*    <div className="flex items-center justify-center py-12">*/}
+                    <Outlet/>
+                {/*</div>*/}
+                {/*<div className="hidden bg-muted lg:block">*/}
+                {/*    <img*/}
+                {/*        src="https://ui.shadcn.com/placeholder.svg"*/}
+                {/*        alt="Image"*/}
+                {/*        width="1920"*/}
+                {/*        height="1080"*/}
+                {/*        className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"*/}
+                {/*    />*/}
+                {/*</div>*/}
+            {/*// </div>*/}
         </BaseLayout>
     )
 }
