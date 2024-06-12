a11636ae481dd3b5b0b27a4bb738d71f681b4b01
Dominik Tichy <tichydominik451@gmail.com>
2024-06-12 17:48:37 +0200

    404 error

--- src/ExpenseManager.Presentation/Client/src/routes/Error.tsx
+++ src/ExpenseManager.Presentation/Client/src/routes/Error.tsx
@@ -1,9 +1,10 @@
 import BaseLayout from "@/layouts/BaseLayout.tsx";
+import {SearchXIcon} from "lucide-react";
 
 export default function Error() {
     return <BaseLayout>
         <div className="flex flex-col items-center justify-center size-full space-y-6">
-
+            <SearchXIcon className="w-20 h-20 text-red-500"/>
             <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
                 404 Not Found
             </h1>
