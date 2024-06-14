d9fecb0783f6abad06cddf418f4dcdfc005e1718
Dominik Tichy <tichydominik451@gmail.com>
2024-06-14 20:36:56 +0200

    added test files

---
+++ tests/UnitTests/ExpenseManager.Application.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace ExpenseManager.Application.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/UnitTests/ExpenseManager.Domain.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace ExpenseManager.Domain.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
